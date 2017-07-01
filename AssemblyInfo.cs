e77c9ead8bb2bbc260fe057a51fbb2f28a46ae63
Mounic Madiraju <mounicraju@gmail.com>
2017-07-01 21:56:03 +0800

    Update AssemblyInfo.cs

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -24,8 +24,6 @@ using System.Runtime.CompilerServices;
 // by using the '*' as shown below:
 
 [assembly: AssemblyVersion("1.0.*")]
-
-//
 // In order to sign your assembly you must specify a key to use. Refer to the 
 // Microsoft .NET Framework documentation for more information on assembly signing.
 //
